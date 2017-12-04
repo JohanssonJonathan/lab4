5931df2574fda86af5c1e2daeaf57d608c4dffc7
thatzita <robinudehn@gmail.com>
2017-12-04 16:27:11 +0100

    more fixes

--- listan.js
+++ listan.js
@@ -12,62 +12,56 @@ console.log(buttons.length)
      function resizeIframe(obj) {
          obj.style.height = obj.contentWindow.document.body.scrollHeight + 'px';
      }
-<<<<<<< HEAD
-     fetch(`https://www.forverkliga.se/JavaScript/api/crud.php?op=select&key=T8T4f`)
-         .then(function (response) {
-             ulList[0].innerHTML = "";
-             error.innerHTML = "";
-             return response.json();
-         }).then(function (json) {
-             if (json.status !== "error") {
-                 for (let i = 0; i < json.data.length; i++) {
-                     let addLi = document.createElement("li");
-                     let button = document.createElement("button");
-                     button.className = "delete btn btn-outline-danger"
-                     button.setAttribute("id", json.data[i].id);
-                     addLi.className = "list-inline-item col-lg-5 col-sm-10 col-md-5";
-                     addLi.innerHTML += `Title: <strong>${json.data[i].title}</strong><br>`;
-                     addLi.innerHTML += `Author: <strong>${json.data[i].author}</strong><br>`;
-                     addLi.innerHTML += `Book ID: <strong class="idvalue">${json.data[i].id}</strong><br>`;
-                     addLi.innerHTML += `Last Update: <strong>${json.data[i].updated}</strong><br><br>`;
-                     addLi.setAttribute("id", json.data[i].title)
-                     button.innerHTML = `Remove`;
-                     addLi.appendChild(button);
-                     let content = ulList[0].appendChild(addLi);
-                     console.log(buttons.length)
-                     console.log(json.data)
-=======
->>>>>>> robin_branch
 
-     getPage();
-     
-  
-
-<<<<<<< HEAD
-             } else {
-
-                 //                 error.innerHTML = `<strong>Ooops, something went wrong!</strong><br> Message: <strong>${json.message}</strong>`
-                 location.reload();
-             }
+//     fetch(`https://www.forverkliga.se/JavaScript/api/crud.php?op=select&key=T8T4f`)
+//         .then(function (response) {
+//             ulList[1].innerHTML = "";
+//             error.innerHTML = "";
+//             return response.json();
+//         }).then(function (json) {
+//             if (json.status !== "error") {
+//                 for (let i = 0; i < json.data.length; i++) {
+//                     let addLi = document.createElement("li");
+//                     let button = document.createElement("button");
+//                     button.className = "delete btn btn-outline-danger"
+//                     button.setAttribute("id", json.data[i].id);
+//                     addLi.className = "list-inline-item col-lg-5 col-sm-10 col-md-5";
+//                     addLi.innerHTML += `Title: <strong>${json.data[i].title}</strong><br>`;
+//                     addLi.innerHTML += `Author: <strong>${json.data[i].author}</strong><br>`;
+//                     addLi.innerHTML += `Book ID: <strong class="idvalue">${json.data[i].id}</strong><br>`;
+//                     addLi.innerHTML += `Last Update: <strong>${json.data[i].updated}</strong><br><br>`;
+//                     addLi.setAttribute("id", json.data[i].title)
+//                     button.innerHTML = `Remove`;
+//                     addLi.appendChild(button);
+//                     let content = ulList[1].appendChild(addLi);
+//                     console.log(buttons.length)
+//                     console.log(json.data)
+//                 }
+//                 else {
+//
+//                 //                 error.innerHTML = `<strong>Ooops, something went wrong!</strong><br> Message: <strong>${json.message}</strong>`
+//                 location.reload();
+//             }
 
 
-             for (let i = 0; i < buttons.length; i++) {
-                 buttons[i].addEventListener("click", function (clicked) {
-                     if (json.status === "success") {
-                         fetch("https://www.forverkliga.se/JavaScript/api/crud.php?op=delete&key=" + "T8T4f" + "&id=" + json.data[i].id)
-                             .then(function (response) {
-                                 return response.json();
+//             for (let i = 0; i < buttons.length; i++) {
+//                 buttons[i].addEventListener("click", function (clicked) {
+//                     if (json.status === "success") {
+//                         fetch("https://www.forverkliga.se/JavaScript/api/crud.php?op=delete&key=" + "T8T4f" + "&id=" + json.data[i].id)
+//                             .then(function (response) {
+//                                 return response.json();
+//
+//                             }).then(function (deleteThis) {
+//                                 console.log(deleteThis.status)
+//                                 if (deleteThis.status === "success") {
+//                                     location.reload();
+//                                 } else {
+//                                     buttons[i].innerHTML = `Error....`;
+//                                 }
+//                             })
 
-                             }).then(function (deleteThis) {
-                                 console.log(deleteThis.status)
-                                 if (deleteThis.status === "success") {
-                                     location.reload();
-                                 } else {
-                                     buttons[i].innerHTML = `Error....`;
-                                 }
-                             })
+                 getPage();
 
-=======
      function getPage(counter = 0) {
          fetch(`https://www.forverkliga.se/JavaScript/api/crud.php?op=select&key=T8T4f`)
              .then(function (response) {
@@ -102,7 +96,7 @@ console.log(buttons.length)
                      }
 
 
->>>>>>> robin_branch
+
 
                      //     document.addEventListener("click", function(event) {
                      //         let getButton = document.getElementsByTagName("button")
@@ -138,12 +132,14 @@ console.log(buttons.length)
                                      })
                              }
 
-<<<<<<< HEAD
-=======
+
                          })
->>>>>>> robin_branch
+
                      }
                  }
              )
      }
  })
+//                 })
+//     })
+                 
