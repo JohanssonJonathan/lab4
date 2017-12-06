38af36d3a83e4ba146d2f1663f752d0d37823912
JohanssonJonathan <jjohanssonjonathan@gmail.com>
2017-12-06 13:25:58 +0100

    okdf

--- index.js
+++ index.js
@@ -6,11 +6,9 @@ window.addEventListener("load", () => {
     //Söka efter bok
     let ulList = document.getElementsByTagName("ul");
     let li = document.getElementsByTagName("li");
-
     let webpage = "https://www.googleapis.com/books/v1/volumes?q="
     let key = "&key=AIzaSyCsuyWFqAI8X9ijyfo1vCSMUGdEwgMnFLc&";
     let clickBtn = document.getElementsByClassName('book');
-
     //////////////////////////////SÖKA/LÄGGA TILL BOK MED API////////////////////////////////////
     searchBookBtn.addEventListener("click", function (event) {
         ulList[1].innerHTML = "";
@@ -21,7 +19,6 @@ window.addEventListener("load", () => {
                 console.log(json.items[2])
                 let idCount = 0;
                 for (let i = 0; i < json.items.length; i++) {
-
                     let button = document.createElement("button");
                     let li = document.createElement("li");
                     li.className = "list-inline-item col-lg-5 col-sm-10 col-md-12";
@@ -31,7 +28,7 @@ window.addEventListener("load", () => {
                     if (json.items[i].volumeInfo.readingModes.image === false) {
                         li.innerHTML += `<img src="./no_book_for_you.png" width="128" alt="Nothing to show">`;
                     } else {
-                        li.innerHTML += `<img src="${json.items[i].volumeInfo.imageLinks.thumbnail} alt="Nothing to show">`;
+                        li.innerHTML += `<img src="${json.items[i].volumeInfo.imageLinks.thumbnail}" alt="Nothing to show">`;
                     }
                     button.innerHTML = `Add to list`;
                     li.appendChild(button);
